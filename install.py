2ce4b589fd70b44638be32744f0645f10b23287b
Karthik <askarthikkumar@gmail.com>
2021-12-16 14:28:11 -0800

    updated

--- install.py
+++ install.py
@@ -181,17 +181,17 @@ post_actions += [
 post_actions += [
     r'''#!/bin/bash
     # install oh-my-zsh
-    if ! -f "$HOME/.dotfiles/ohmyzsh/oh-my-zsh.sh" > /dev/null; then
+    if [ ! -f "$HOME/.dotfiles/ohmyzsh/oh-my-zsh.sh" ]; then
         echo -e "\033[0;31mError: oh-my-zsh not found. Please install oh-my-zsh.\033[0m";
         read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
         ZSH="$HOME/.dotfiles/zsh/oh-my-zsh"
         KEEP_ZSHRC="yes"
 	    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     fi
-    if ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" > /dev/null; then
+    if [ ! -f "$HOME/.oh-my-zsh/oh-my-zsh.sh" ]; then
         ln -s "$HOME/.dotfiles/zsh/oh-my-zsh" "$HOME/.oh-my-zsh"
     fi
-    if ! -d "$HOME/.oh-my-zsh/plugins/fzf-tab" > /dev/null; then
+    if [ ! -d "$HOME/.oh-my-zsh/plugins/fzf-tab" ]; then
         ln -s "$HOME/.dotfiles/zsh/fzf-tab" "$HOME/.oh-my-zsh/plugins/fzf-tab"
     fi
 ''']
